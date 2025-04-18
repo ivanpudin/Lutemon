144b7336a694bffec9e553683dae34c5ff56ce15
Ivan Pudin <ivanpudin9@gmail.com>
2025-04-18 16:21:14 +0300

    Add lutemon delete feature

--- app/src/main/java/com/example/lutemon/LutemonStatisticActivity.java
+++ app/src/main/java/com/example/lutemon/LutemonStatisticActivity.java
@@ -3,6 +3,7 @@ package com.example.lutemon;
 import android.os.Bundle;
 
 import com.example.lutemon.model.Lutemon;
+import com.example.lutemon.model.LutemonStorage;
 import com.example.lutemon.util.ErrorHandler;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -11,24 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class LutemonStatisticActivity extends AppCompatActivity {
     // Intent extras
     public static final String EXTRA_LUTEMON = "extra_lutemon";
-    private ImageView statisticViewImage;
-    private TextView statisticViewAttack, statisticViewHealth;
-    private FloatingActionButton backButton;
+    private ImageView statisticViewImage, statisticViewElement;
+    private TextView statisticViewName, statisticViewAttack, statisticViewHealth;
+    private FloatingActionButton backButton, deleteButton;
+    private Lutemon lutemon;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lutemon_statistic);
 
         statisticViewImage = findViewById(R.id.statisticViewImage);
+        statisticViewElement = findViewById(R.id.statisticViewElement);
+        statisticViewName = findViewById(R.id.statisticViewName);
         statisticViewAttack = findViewById(R.id.statisticViewAttack);
         statisticViewHealth = findViewById(R.id.statisticViewHealth);
         backButton = findViewById(R.id.statisticViewBackButton);
+        deleteButton = findViewById(R.id.statisticViewDeleteButton);
 
         displayLutemonDetails();
 
@@ -39,18 +44,30 @@ public class LutemonStatisticActivity extends AppCompatActivity {
             }
         });
 
+        deleteButton.setOnClickListener(v -> {
+            if (lutemon != null) {
+                LutemonStorage.getInstance().removeLutemonById(lutemon.getId());
+                Toast.makeText(this, "Lutemon deleted.", Toast.LENGTH_SHORT).show();
+
+                setResult(RESULT_OK);
+                finish(); // Close activity
+            } else {
+                Toast.makeText(this, "Failed to delete lutemon.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void displayLutemonDetails() {
         try {
             // Get lutemon from the intent
-            Lutemon lutemon = (Lutemon) getIntent().getSerializableExtra(EXTRA_LUTEMON);
+            lutemon = (Lutemon) getIntent().getSerializableExtra(EXTRA_LUTEMON);
 
             if (lutemon == null) {
                 throw new IllegalArgumentException("Lutemon not found");
             }
 
             // Display lutemon details
+            statisticViewName.setText(lutemon.getName());
             String attackDice = lutemon.getAttackCount() + "d" + lutemon.getAttackDice();
             statisticViewAttack.setText(attackDice);
             String currentHealth = lutemon.getCurrentHealth() + "/" + lutemon.getMaxHealth();
@@ -64,6 +81,16 @@ public class LutemonStatisticActivity extends AppCompatActivity {
             } catch (Exception e) {
                 // If all else fails, set a background color
                 statisticViewImage.setBackgroundColor(
+                        getResources().getColor(android.R.color.darker_gray));
+            }
+
+            try {
+                int imageResourceId = ErrorHandler.getDrawableResourceId(
+                        this, lutemon.getElementIconResource(), R.drawable.element_not_found);
+                statisticViewElement.setImageResource(imageResourceId);
+            } catch (Exception e) {
+                // If all else fails, set a background color
+                statisticViewElement.setBackgroundColor(
                         getResources().getColor(android.R.color.darker_gray));
             }
 
